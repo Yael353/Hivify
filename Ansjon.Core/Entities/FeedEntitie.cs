573bf23c6880be026e2664ec248625a7b9a8215a
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2026-06-11 15:13:33 +0200

    Add AuthorId property to FeedEntitie class
    Introduced a new Guid property, AuthorId, to the FeedEntitie class in the Ansjon.Core.Entities namespace to associate each feed entity with its author.

--- Ansjon.Core/Entities/FeedEntitie.cs
+++ Ansjon.Core/Entities/FeedEntitie.cs
@@ -10,5 +10,6 @@ namespace Ansjon.Core.Entities
         public required string Title { get; set; }
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public Guid AuthorId { get; set; }
     }
 }
