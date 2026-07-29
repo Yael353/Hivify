056b369bdd5ab9dbe6af55c096010729d04b76c5
odinous95 <mjd.thif@icloud.com>
2026-07-29 14:21:13 +0100

    Add current association provider for association context

---
+++ Ansjon.UseCases/Abstractions/Context/ICurrentAssociationProvider.cs
@@ -0,0 +1,10 @@
+﻿using Ansjon.Core.Aggregates.Associations;
+
+namespace Ansjon.UseCases.Abstractions.Context
+{
+    public interface ICurrentAssociationProvider
+    {
+        Task<AssociationID> GetAssociationIdAsync(
+            CancellationToken cancellationToken = default);
+    }
+}
