d1a76873a7587e027278a4fdd16d3db44c40fcb9
odinous95 <mjd.thif@icloud.com>
2026-07-29 14:20:33 +0100

    Add association repository implementation

---
+++ Ansjon.Infrastructures/Repositories/AssociationRepo/AssociationRepo.cs
@@ -0,0 +1,43 @@
+﻿using Ansjon.Core.Aggregates.Associations;
+using Ansjon.Infrastructures.SqlDatabase;
+using Ansjon.UseCases.Abstractions.Presistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ansjon.Infrastructures.Repositories.AssociationRepo
+{
+    public sealed class AssociationRepository : IAssociationRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AssociationRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public async Task<AssociationEntity?> GetByIdAsync(
+            AssociationID id,
+            CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Associations
+                .FirstOrDefaultAsync(
+                    a => a.Id == id,
+                    cancellationToken);
+        }
+
+        public async Task AddAsync(
+            AssociationEntity association,
+            CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Associations.AddAsync(association, cancellationToken);
+        }
+
+        public async Task SaveChangesAsync(
+            CancellationToken cancellationToken = default)
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+
+}
