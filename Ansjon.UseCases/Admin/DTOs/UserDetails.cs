a4079bcd006e4556b26489fb51543878bab5a27e
odinous95 <mjd.thif@icloud.com>
2026-08-11 12:56:38 +0100

    Added userlist to to admin sidebar

--- Ansjon.UseCases/Admin/DTOs/UserDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ansjon.UseCases.Admin.DTOs
-{
-    public sealed record UserDetails(
-     Guid UserId,
-     string? UserName,
-     string? Email,
-     bool EmailConfirmed,
-     bool LockedOut);
-}
