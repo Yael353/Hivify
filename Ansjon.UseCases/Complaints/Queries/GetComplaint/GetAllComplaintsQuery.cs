6d20b52f01ca2766884d1e69fa1d47d5bd857347
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2026-08-19 11:33:25 +0200

    Refactor complaints UI, add admin view & query handler
    Introduce GetAllComplaintsQuery and handler for admin to fetch all complaints; add AdminComplaints Blazor component with status chips and actions. Update navigation: user "My Complaints" link, admin path to "/complaint". Refactor complaint submission by consolidating form logic into ComplaintComponent, removing modular form components and their CSS. Improve file upload, form reset, and navigation logic. Home page now links to complaint management with MudBlazor styling.

---
+++ Ansjon.UseCases/Complaints/Queries/GetComplaint/GetAllComplaintsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Ansjon.UseCases.Abstractions.Messaging;
+using Ansjon.UseCases.Complaints.DTOs;
+
+
+namespace Ansjon.UseCases.Complaints.Queries.GetComplaint
+{
+    public sealed record GetAllComplaintsQuery() : IQuery<IReadOnlyList<ComplaintListItemDto>>;
+}
