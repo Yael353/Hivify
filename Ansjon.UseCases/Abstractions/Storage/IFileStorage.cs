3bd2f04fc5f0e3c5b371cb6cd011ae2f7f036dae
odinous95 <mjd.thif@icloud.com>
2026-08-24 09:54:43 +0100

    feat: IFileStorage Interface for implementing Storge for uploading and listing files

---
+++ Ansjon.UseCases/Abstractions/Storage/IFileStorage.cs
@@ -0,0 +1,17 @@
+﻿using Ansjon.UseCases.Documents.Results;
+
+namespace Ansjon.UseCases.Abstractions.Storage
+{
+    public interface IFileStorage
+    {
+        Task<FileUploadResult> UploadAsync(
+            Stream stream,
+            string fileName,
+            string contentType,
+            CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<FileDocumentResult>> GetAllAsync(
+    CancellationToken cancellationToken = default);
+    }
+
+
+}
