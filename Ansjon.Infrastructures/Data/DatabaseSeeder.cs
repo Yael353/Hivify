7f2c7b7e32fad1d665a229112ca8d640892c8efb
odinous95 <mjd.thif@icloud.com>
2026-08-24 10:54:14 +0100

    feat: seeding houses

--- Ansjon.Infrastructures/Data/DatabaseSeeder.cs
+++ Ansjon.Infrastructures/Data/DatabaseSeeder.cs
@@ -1,7 +1,6 @@
 ﻿using Ansjon.Core.Aggregates.Associations;
 using Ansjon.Core.Aggregates.Associations.Members;
 using Ansjon.Core.Aggregates.Feeds;
-using Ansjon.Core.Aggregates.Houses;
 using Ansjon.Core.SharedKernel.ValuesObjects;
 using Ansjon.Infrastructures.SqlDatabase;
 using Microsoft.EntityFrameworkCore;
@@ -18,27 +17,24 @@ public static class DatabaseSeeder
             context.Feeds.Add(
                 Feed.CreateFeed(
                     new MemberID(Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa")),
-                    MemberRole.CommiteAnsvarig,
+                    MemberRole.StyrelseLedamot,
                     new Title("Welcome to Ansjon"),
                     new Description("This is the first seeded feed.")));
-
 
             context.Feeds.Add(
                 Feed.CreateFeed(
                     new MemberID(Guid.Parse("cccccccc-cccc-cccc-cccc-cccccccccccc")),
-                    MemberRole.CommiteAnsvarig,
+                    MemberRole.StyrelseLedamot,
                     new Title("Getting Started"),
                     new Description("Learn how to use the platform.")));
         }
-
 
         // Seed Association
         Association? association = null;
 
         if (!await context.Associations.AnyAsync())
         {
-            association = Association.Create(
-                "Ansjon Housing Association");
+            association = Association.Create("Ansjon Housing Association");
 
             context.Associations.Add(association);
         }
@@ -47,30 +43,9 @@ public static class DatabaseSeeder
             association = await context.Associations.FirstAsync();
         }
 
+        await context.SaveChangesAsync();
 
         // Seed Houses
-        if (!await context.Houses.AnyAsync())
-        {
-            var house1 = House.Create(
-
-                new Address("Main Street"),
-                new HouseNumber("10"),
-                new PostalCode("21120"));
-
-
-            var house2 = House.Create(
-
-                new Address("Park Avenue"),
-                new HouseNumber("25"),
-                new PostalCode("21130"));
-
-
-            context.Houses.AddRange(
-                house1,
-                house2);
-        }
-
-
-        await context.SaveChangesAsync();
+        await HouseSeeder.SeedAsync(context);
     }
 }
